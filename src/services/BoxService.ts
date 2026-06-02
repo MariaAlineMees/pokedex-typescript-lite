bf2018280981d423b5cef95b631e77b5bcd9de0a
Maria Aline Mees <mariaalinemees@gmail.com>
2026-06-02 13:03:29 -0300

    feat: ordena a listagem do catalogo por ID

--- src/services/BoxService.ts
+++ src/services/BoxService.ts
@@ -34,13 +34,15 @@ export class CatalogoPokemon {
     await this.salvarNoArquivo();
   }
 
-  listar(): void {
+listar(): void {
     if (this.pokemons.length === 0) {
       console.log("[AVISO] Catálogo vazio.");
       return;
     }
 
-    console.log("Catálogo atual:");
+    this.pokemons.sort((a, b) => a.id - b.id);
+
+    console.log("Catálogo atual (Ordenado por ID):");
     this.pokemons.forEach((pokemon) => {
       console.log(
         `#${pokemon.id} ${pokemon.nome} | Tipos: ${pokemon.tipos.join(", ")} | Altura: ${pokemon.altura} | Peso: ${pokemon.peso}`
